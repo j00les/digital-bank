3e9149dedf1575fd8b9eda06ada9880f23494433
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-08 19:14:57 +0700

    Display movement funcction

--- script.js
+++ script.js
@@ -61,6 +61,40 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const element = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+      `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', element);
+  });
+};
+displayMovements(account1.movements);
+const createUsername = accounts => {
+  accounts.forEach(acc => {
+    acc['username'] = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+const usd = 1.1;
+const depositInUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * usd)
+  .reduce((acc, curr) => acc + curr, 0);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,18 +108,3 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
-const createUsername = accounts => {
-  accounts.forEach(acc => {
-    acc['username'] = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(name => name[0])
-      .join('');
-  });
-};
-const usd = 1.1;
-const depositInUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * usd)
-  .reduce((acc, curr) => acc + curr, 0);

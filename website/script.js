f6b32a1b9cdc8f159b350dd98d875ae10504d1d2
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-14 08:01:37 +0700

    Animate sections

--- website/script.js
+++ website/script.js
@@ -118,7 +118,31 @@ const stickyNav = function (entries) {
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0,
+  threshold: 0, 
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+//reveal sections
+const allSections = document.querySelectorAll('.section')
+
+const revealSections = function(entries, observer) {
+  const [entry] = entries
+  console.log(entry);
+
+  //guard clause
+  if(!entry.isIntersecting) return 
+
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.15 //15 precent
+})
+
+allSections.forEach(section => {
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
+})
